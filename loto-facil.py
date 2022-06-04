6a6fcdce210e602ba3c9fabbdc5f26b0e4885bda
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-04 14:21:47 -0300

    adicionando comentario

--- loto-facil.py
+++ loto-facil.py
@@ -15,14 +15,15 @@ print('-' * 30)
 
 # Pergunta
 quantidade = int(input('Quantos jogos quer sortear? '))
-""" Quantidade de jogos """
+""" Quantidade de jogos. """
 total = 1
 
 # Sorteio
 while total <= quantidade:
     contagem = 0
-    """ Contagem de jogos """
+    """ Contagem de jogos. """
     while True:
-        """ Sorteio de números """
+        """ Sorteio de números. """
         numero = randint(1, 25)
         if numero not in lista:
+            """ Verifica se o número já existe. """
